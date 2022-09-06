88f15c5fb431b97fec2b0cfe68b8f3e37ab65b9c
Hann Hopp <hhopp@arena.net>
2022-09-06 16:30:24 -0700

    playwright expect rule; refactor report to take in a fixer

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const errors = {
     ERROR_MITHRIL_VIEW_MUST_RETURN : "Mithril view must return",
     ERROR_MSG_NO_REQ_FOUND : "Reply must respond with request",
+    ERROR_MUST_AWAIT_PLAYWRIGHT_EXPECT : "Playwright expect must use await",
     ERROR_USE_STRICT_AT_TOP : "use strict must be at top of document"
 };
 
-function report(context, node, message) {
+/**
+ * This is the error that shows up in the tooltip and CLI when eslint is running.
+ * @param {object} context - eslint context, helpful for rules to do their job
+ * @param {object} node - node being looked at in the AST
+ * @param {string} message - error message
+ * @param {function} fixer - autofixes the code
+ * @returns
+ */
+function report({ context, node, message, fix = new Function() }) {
     return context.report({
         node,
-        message
+        message,
+        fix
     });
 }
 
@@ -22,7 +32,7 @@ module.exports = {
                         }
 
                         if (node.value.type !== "FunctionExpression" && node.value.type !== "ArrowFunctionExpression") {
-                            return report(context, node, errors.ERROR_MITHRIL_VIEW_MUST_RETURN);
+                            return report({ context, node, message : errors.ERROR_MITHRIL_VIEW_MUST_RETURN });
                         }
 
                         function returnFound(body = []) {
@@ -49,11 +59,44 @@ module.exports = {
                             const returnStatement = returnFound(node.value.body.body);
 
                             if (!returnStatement) {
-                                return report(context, node, errors.ERROR_MITHRIL_VIEW_MUST_RETURN);
+                                return report({ context, node, message : errors.ERROR_MITHRIL_VIEW_MUST_RETURN });
                             }
                         }
                     }
                 };
+            }
+        },
+        "must-await-playwright-expect" : {
+            meta : {
+                type : "problem",
+                docs : "Playwright's `expect` can be sync or async. This rule enforces the use of the async pattern to avoid confusing behavior.",
+                fixable : "code"
+            },
+            create : function(context) {
+                return {
+                    ExpressionStatement(node) {
+                        const isExpect = node.expression &&
+                              node.expression.callee &&
+                              node.expression.callee.object &&
+                              node.expression.callee.object.callee &&
+                              node.expression.callee.object.callee.name === "expect";
+
+                        if (!isExpect) {
+                          return;
+                        }
+
+                        if (isExpect && node.expression.type !== "AwaitExpression") {
+                            return report({
+                                context,
+                                node,
+                                message : errors.ERROR_MUST_AWAIT_PLAYWRIGHT_EXPECT,
+                                fix : function(fixer) {
+                                    return fixer.insertTextBefore(node, "await ");
+                                }
+                            });
+                        }
+                    }
+                  };
             }
         },
         "reply-with-request" : {
@@ -83,19 +126,19 @@ module.exports = {
                         }
 
                         if (!arg) {
-                            return report(context, node, errors.ERROR_MSG_NO_REQ_FOUND);
+                            return report({ context, node, message : errors.ERROR_MSG_NO_REQ_FOUND });
                         }
 
                         const properties = arg.properties;
 
                         if (!properties) {
-                            return report(context, node, errors.ERROR_MSG_NO_REQ_FOUND);
+                            return report({ context, node, message : errors.ERROR_MSG_NO_REQ_FOUND });
                         }
 
                         const hasReq = Boolean(properties.filter((prop) => prop.key && prop.key.name === "req").length);
 
                         if (!hasReq) {
-                            return report(context, node, errors.ERROR_MSG_NO_REQ_FOUND);
+                            return report({ context, node, message : errors.ERROR_MSG_NO_REQ_FOUND });
                         }
                     }
                 };
@@ -112,11 +155,11 @@ module.exports = {
                         }
 
                         if (firstChild.type !== "ExpressionStatement") {
-                            return report(context, node, errors.ERROR_USE_STRICT_AT_TOP);
+                            return report({ context, node, message : errors.ERROR_USE_STRICT_AT_TOP });
                         }
 
                         if (firstChild.expression.value !== "use strict") {
-                            return report(context, node, errors.ERROR_USE_STRICT_AT_TOP);
+                            return report({ context, node, message : errors.ERROR_USE_STRICT_AT_TOP });
                         }
                     }
                 };
